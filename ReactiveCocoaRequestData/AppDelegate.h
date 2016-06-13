f237e296c5a04f45e35bb13fbf6e4c377a43f04d
MyJohnson <voidxin@wugumofang.local>
2016-06-13 14:02:07 +0800

    Initial Commit

---
+++ ReactiveCocoaRequestData/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ReactiveCocoaRequestData
+//
+//  Created by voidxin on 16/6/13.
+//  Copyright © 2016年 wugumofang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ReactiveCocoaRequestData/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ReactiveCocoaRequestData
+//
+//  Created by voidxin on 16/6/13.
+//  Copyright © 2016年 wugumofang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
